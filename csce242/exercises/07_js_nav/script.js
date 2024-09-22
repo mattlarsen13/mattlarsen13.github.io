748eb29c898cbd410dbf2c1578e09deba1627d43
Matt Larsen <matthew.larsen13@gmail.com>
2024-09-22 14:50:39 -0400

    Sep 22

--- csce242/exercises/07_js_nav/script.js
+++ csce242/exercises/07_js_nav/script.js
@@ -1,21 +1,20 @@
-/* When the hamburger is clicked, toggle between showing and hiding the nav items */
-//document.getElementById("toggle-nav").onclick = () => 
-    //OR
-
+/* When the hamburger is clicked, toggle between showing
+and hiding the nav items */
+//document.getElementById("toggle-nav")
 document.querySelector("#toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hidden-small");
 };
 
-/* Dont go to link destination when clicked */
+/* Don't go to links desitination when clicked */
 document.getElementById("click-link").onclick = (event) => {
-    event.preventDefault(); // Dont go to links destination
-    console.log("You clicked a link");
+    event.preventDefault(); //don't go to the links destination
+    console.log("you clicked a link");
 };
 
-/* Change ball color */
+/* Change the ball color */
 document.getElementById("txt-color").onchange = (e) => {
-   // document.getElementById("ball").style.setProperty("background-color", e.target.value);
-   document.querySelector(":root").style.setProperty("--ball-color", e.target.value); //Using a root variable instead
+    //document.getElementById("ball").style.setProperty("background-color", e.target.value);
+    document.querySelector(":root").style.setProperty("--ball-color", e.target.value);
 }
 
 /* Move the ball down when the button is clicked */
@@ -23,6 +22,47 @@ let pos = 0;
 document.getElementById("move-down").onclick = () => {
     pos++;
     document.getElementById("ball").style.setProperty("top", pos + "px");
+}
+
+/* Show color message */
+document.getElementById("btn-show-color").onclick = () => {
+    const color = document.getElementById("txt-enter-color").value.toLowerCase().trim();
+    const messageP = document.getElementById("color-message");
+    let mood = "";
+
+    if(color == "red"){
+        mood = "angry";
+    }
+    else if(color == "blue"){
+        mood = "moody";
+    }
+    else {
+        mood = "undefined";
+    }
+
+    messageP.innerHTML = `You chose ${color} so you are ${mood}`;
+}
+
+/* Donations */
+/* Get the number
+- If it is not a number or it is negative, show an error in the error
+Otherwise First show the percent out of 10000 towards goal
+Next show a visual representation with a box and a gradient */
+document.getElementById("btn-donate").onclick = () => {
+    const goal = 10000;
+    const donation = document.getElementById("txt-donations").value;
+    const error = document.getElementById("error-donation");
+    error.innerHTML = "";   //clear previous error
+
+    //if we got an invalid donation -> not a number or less than 0
+    if(isNaN(donation) || donation < 0){
+        error.innerHTML = "* Invalid";
+        return;
+    }
+   
+    const percentGoal = donation / goal * 100;
+    console.log(percentGoal + "%");
+    document.querySelector(":root").style.setProperty("--funds", percentGoal + "%");
 }
 
 
