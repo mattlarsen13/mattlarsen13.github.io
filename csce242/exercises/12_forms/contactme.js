8e7cfa42446c13735b0d42808e1cf36815e0d4b9
Matt Larsen <matthew.larsen13@gmail.com>
2024-10-15 12:45:09 -0400

    Oct 15 pt2

--- csce242/exercises/12_forms/contactme.js
+++ csce242/exercises/12_forms/contactme.js
@@ -1,4 +1,4 @@
-/*
+const sendEmail = async(json) => {
     try {
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
@@ -14,4 +14,23 @@
         console.log(error);
         result.innerHTML = "Sorry, your email couldn't be sent";
     }
-*/
+};
+
+document.getElementById("contact-form").onsubmit = async(e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Sending..."
+
+    const httpResult = await sendEmail(json);
+
+    if(httpResult.status == 200) {
+        result.innerHTML = "email successfully sent"
+    } else [
+        result.innerHTML = "email not sent"
+    ]
+}
--- csce242/exercises/12_forms/form.js
+++ csce242/exercises/12_forms/form.js
@@ -1,4 +1,23 @@
 document.getElementById("form-raccoon").onsubmit = (e) => {
     e.preventDefault(); // dont go to action .. dont refresh page
-    console.log("submitting");
-}
+
+    const form = e.target;
+
+    const raccoonName = form.elements["raccoon-name"].value;
+    const demeanor = form.elements["demenaor"].value;
+    const termsChecked = form.elements["terms"].checked;
+    const size = getRadioValue("size");
+    console.log();
+};
+
+const getRadioValue = (radioName) => {
+    const radios = document.getElementsByName(radioName);
+
+    for(let i in radios) {
+        if(radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+
+    return "";
+};
