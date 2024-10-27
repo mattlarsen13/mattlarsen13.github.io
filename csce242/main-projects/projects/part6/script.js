1d4af5d057917218a11b5868c88a749d2259414f
Matt Larsen <matthew.larsen13@gmail.com>
2024-10-27 00:59:03 -0400

    Oct 27

--- csce242/main-projects/projects/part6/script.js
+++ csce242/main-projects/projects/part6/script.js
@@ -55,3 +55,42 @@ const showPlayers = async () => {
 };
 
 showPlayers();
+
+const sendEmail = async(json) => {
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+        });
+
+        return response;
+    } catch(error){
+        console.log(error);
+        result.innerHTML = "Sorry, your email couldn't be sent";
+    }
+};
+
+document.getElementById("form").onsubmit = async(e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Sending..."
+
+    const httpResult = await sendEmail(json);
+
+    if(httpResult.status == 200) {
+        result.innerHTML = "Email successfully sent!"
+        result.style.color = "green"
+    } else {
+        result.innerHTML = "Email not sent..."
+        result.style.color = "red"
+    }
+}
